f6352bd46d854ac9066559a11013017c4da98d21
KodeWithKK <kodewithkk@gmail.com>
2024-02-14 14:01:46 +0530

    Repo Updated

--- src/Frontend/js/script.js
+++ src/Frontend/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const serverURL = 'https://k3url.vercel.com/';
+const serverURL = 'https://k3url.vercel.com';
 // const serverURL = 'https://k3url.onrender.com';
 // const serverURL = 'http://localhost:8000';
 
