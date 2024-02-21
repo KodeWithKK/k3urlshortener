f7cd51d752a0c72fd5eae2315ba375aeeda9d0db
KodeWithKK <kodewithkk@gmail.com>
2024-02-21 19:26:33 +0530

    Fixed Some minor issues

--- src/Frontend/js/script.js
+++ src/Frontend/js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // const serverURL = 'https://k3url.vercel.com';
-// const serverURL = "https://k3url.onrender.com";
-const serverURL = "http://localhost:8000";
+const serverURL = "https://k3url.onrender.com";
+// const serverURL = "http://localhost:8000";
 
 import { loadData, addUrlBar, saveHistory, getHistory } from "./utils.js";
 
