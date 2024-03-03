089d91fb33a02fabbb68f24fe4e09d87030b3c7e
KodeWithKK <kodewithkk@gmail.com>
2024-03-03 22:41:28 +0530

    fixed some minor bugs

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-🔗 Live URL: [Click Here](https://k3url.onrender.com/)
+🔗 Live URL: [Click Here](https://k3url.vercel.app/)
--- client/js/const.js
+++ client/js/const.js
@@ -1,4 +1,4 @@
-const serverURL = "https://k3url.vercel.com";
+const serverURL = "https://k3url.vercel.app";
 // const serverURL = "https://k3url.onrender.com";
 // const serverURL = "http://localhost:8000";
 
